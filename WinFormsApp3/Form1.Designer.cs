b7515de4747add6b92fdcb0fa513aef2bd748373
AlexProstolucky <alexprostolucky@gmail.com>
2023-10-13 16:46:44 +0300

    Добавьте файлы проекта.

---
+++ WinFormsApp3/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            button1 = new Button();
+            button2 = new Button();
+            process1 = new System.Diagnostics.Process();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Items.AddRange(new object[] { "Notepad", "Calculator" });
+            listBox1.Location = new Point(12, 12);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(295, 424);
+            listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 15;
+            listBox2.Location = new Point(493, 12);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(295, 424);
+            listBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(350, 78);
+            button1.Name = "button1";
+            button1.Size = new Size(113, 32);
+            button1.TabIndex = 2;
+            button1.Text = "Start";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(350, 199);
+            button2.Name = "button2";
+            button2.Size = new Size(113, 32);
+            button2.TabIndex = 3;
+            button2.Text = "Stop";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // process1
+            // 
+            process1.StartInfo.Domain = "";
+            process1.StartInfo.LoadUserProfile = false;
+            process1.StartInfo.Password = null;
+            process1.StartInfo.StandardErrorEncoding = null;
+            process1.StartInfo.StandardInputEncoding = null;
+            process1.StartInfo.StandardOutputEncoding = null;
+            process1.StartInfo.UserName = "";
+            process1.SynchronizingObject = this;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(listBox2);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private Button button1;
+        private Button button2;
+        private System.Diagnostics.Process process1;
+    }
+}
---
+++ WinFormsApp3/Form1.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Diagnostics;
+
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process programProcess = new Process();
+            Hashtable openWith = new Hashtable
+            {
+                { "Notepad", "notepad.exe" },
+                { "Calculator", "calc.exe" },
+            };
+            string text = listBox1.Text;
+            string path = (string)openWith[text];
+            programProcess.StartInfo = new ProcessStartInfo(path);
+            try
+            {
+                programProcess.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка при завершенні процесу: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            listBox2.Items.Add(text);
+            listBox1.Items.Remove(text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string text = listBox2.Text;
+            Hashtable openWith = new Hashtable
+            {
+                { "Notepad", "notepad" },
+                { "Calculator", "CalculatorApp" },
+            };
+            string path = (string)openWith[text];
+            try
+            {
+                Process[] processes = Process.GetProcessesByName(path);
+                foreach (Process process in processes)
+                {
+                    process.Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка при завершенні процесу: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            listBox1.Items.Add(text);
+            listBox2.Items.Remove(text);
+        }
+    }
+}
